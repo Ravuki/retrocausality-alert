0b09d8a7aca104ca846841cef9de2e3acfdef717
Ravuki <qbanieweglowski@gmail.com>
2026-08-03 16:17:10 +0200

    Update retro_alert.py

--- retro_alert.py
+++ retro_alert.py
@@ -1,7 +1,8 @@
 import requests
+import os
 from datetime import datetime
 
-WEBHOOK_URL = "TUTAJ_PÓŹNIEJ"
+WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
 
 def send_alert(text):
     requests.post(
@@ -10,3 +11,5 @@ def send_alert(text):
     )
 
 print("Bot działa:", datetime.now())
+
+send_alert("✅ GitHubowy bot Retrocausality działa!")
